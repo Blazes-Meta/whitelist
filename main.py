fed43111200582cec39b13de9727872b0de5444e
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-10-01 13:37:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+import os
+import asyncio
+import discord
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+from acemeta import log
 
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+async def loadCogs():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+            log(f"[COGS] cogs/{filename} is loaded")
+
+async def main():
+    load_dotenv() # Läd die Umgebungsvariabeln
+    async with bot:
+        await loadCogs()
+        await bot.start(str(os.getenv("BOT_TOKEN")))
+
+@bot.event
+async def on_ready():
+    log(f"[AUTH] Bot is connected")
+    log(f"[AUTH] Logged in as {bot.user} (ID: {bot.user.id})")
+
+asyncio.run(main()) # Diese Zeile wird fortlaufend ausgeführt und sollte deswegen am Ende stehen
