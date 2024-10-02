b925d30306f46704c7d1b6f5ad4ccfee6786d9b2
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-10-02 17:54:11 +0200

    Update playerbase.py

--- cogs/playerbase.py
+++ cogs/playerbase.py
@@ -1,6 +1,8 @@
 OPERATORS = [720992368110862407]
+DATABASE = 'playerbase.db'
 
 import sqlite3
+import requests
 import discord
 from discord.ext import commands
 
@@ -13,110 +15,104 @@ class Playerbase(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        log(f"[COGS] {__name__} is ready")
+        print(f"[COGS] {__name__} is ready")
 	    
-	conn = sqlite3.connect('playerbase.db')
+        conn = sqlite3.connect(DATABASE)
         cursor = conn.cursor()
         cursor.execute('CREATE TABLE IF NOT EXISTS player (DcID INTEGER PRIMARY KEY, UUID TEXT NOT NULL)')
         conn.commit()
         conn.close()
 
-    #-------------------------------------------------#
-    #                   Mojang-API                    #
-    #-------------------------------------------------#
-    MOJANG_API = "https://api.mojang.com/users/profiles/minecraft"
-    MOJANG_SESSIONSERVER = f"https://sessionserver.mojang.com/session/minecraft/profile"
-
-    class APIError(Exception): ...
-	
-    def getUUID(playername: str) -> str:
-	response = requests.get(f"{MOJANG_API}/{playername}")
-	if response.status_code == 200:
-            data = response.json()
-            return data['id']
-        raise APIError
-	    
-    def getPlayername(uuid: str) -> str:
-        response = requests.get(f"{MOJANG_SESSIONSERVER}/{uuid}")
-        if response.status_code == 200:
-            data = response.json()
-            return data['name']
-        raise APIError
-
-    #-------------------------------------------------#
-    #                  DB-Interface                   #
-    #-------------------------------------------------#
-
-    class NoEntry(Exception): ...
-
-    def playerExists(dcid: int) -> bool:
-	conn = sqlite3.connect('playerbase.db')
-        cursor = conn.cursor()
-        cursor.execute("SELECT 1 FROM player WHERE DcID = ?", (dcid,))
-        result = cursor.fetchone()
-	conn.close()
-	if result is None
-            return False
-	return True
-
-    def playerbaseSet(dcid: int, playername: str) -> None:
-	uuid = getUUID(playername)
-	if not playerExists(DcID):
-	    conn = sqlite3.connect('playerbase.db')
-            cursor = conn.cursor()
-            cursor.execute("INSERT INTO player (DcID, UUID) VALUES (?, ?)", (dcid, uuid))
-	    conn.commit()
-	    conn.close()
-	else:
-	    conn = sqlite3.connect('playerbase.db')
-            cursor = conn.cursor()
-            cursor.execute("UPDATE player SET UUID = ? WHERE DcID = ?", (uuid, dcid))
-	    conn.commit()
-	    conn.close()
-	    
-    def playerbaseRemove(dcid: int) -> None:
-	if playerExists(dcid):
-	    conn = sqlite3.connect('playerbase.db')
-            cursor = conn.cursor()
-            cursor.execute("DELETE FROM player WHERE DcID = ?", (dcid,)
-	    conn.commit()
-	    conn.close()
-	else:
-	    raise NoEntry
-	    
-    def playerbaseList() -> dict:
-	...
-	    
-
-    #-------------------------------------------------#
-    #                   Dc-Commands                   #
-    #-------------------------------------------------#
-
-    class PermissionError(Exception): ...
-
     @commands.command()
     async def playerbase(self, ctx, arg1=None, arg2=None):
-	userid = ctx.author.id
-	OPERATORS = OPERATORS.append(userid)
+
+        #-------------------------------------------------#
+        #                   Mojang-API                    #
+        #-------------------------------------------------#
+        MOJANG_API = "https://api.mojang.com/users/profiles/minecraft"
+        MOJANG_SESSIONSERVER = f"https://sessionserver.mojang.com/session/minecraft/profile"
+        
+        def getUUID(playername: str) -> str:
+            response = requests.get(f"{MOJANG_API}/{playername}")
+            if response.status_code == 200:
+                data = response.json()
+                return data['id']
+            raise Exception("UUID konnte nicht erhalten werden")
+            
+        def getPlayername(uuid: str) -> str:
+            response = requests.get(f"{MOJANG_SESSIONSERVER}/{uuid}")
+            if response.status_code == 200:
+                data = response.json()
+                return data['name']
+            raise Exception("Spielername konnte nicht erhalten werden")
+
+        #-------------------------------------------------#
+        #                  DB-Interface                   #
+        #-------------------------------------------------#
+
+        class NoEntry(Exception): ...
+
+        def playerExists(dcid: int) -> bool:
+            conn = sqlite3.connect('playerbase.db')
+            cursor = conn.cursor()
+            cursor.execute("SELECT 1 FROM player WHERE DcID = ?", (dcid,))
+            result = cursor.fetchone()
+            conn.close()
+            if result is None:
+                return False
+            return True
+
+        def playerbaseSet(dcid: int, playername: str) -> None:
+            uuid = getUUID(playername)
+            conn = sqlite3.connect('playerbase.db')
+            cursor = conn.cursor()
+            if not playerExists(dcid):
+                cursor.execute("INSERT INTO player (DcID, UUID) VALUES (?, ?)", (dcid, uuid))
+            else:
+                cursor.execute("UPDATE player SET UUID = ? WHERE DcID = ?", (uuid, dcid))
+            conn.commit()
+            conn.close()
+            
+        def playerbaseRemove(dcid: int) -> None:
+            if playerExists(dcid):
+                conn = sqlite3.connect('playerbase.db')
+                cursor = conn.cursor()
+                cursor.execute("DELETE FROM player WHERE DcID = ?", (dcid,))
+                conn.commit()
+                conn.close()
+            else:
+                raise Exception("Kein Spieler vorhanden")
+            
+        def playerbaseList() -> dict:
+            ...
 	    
-	if arg1 == "set":
-	    if arg2 in OPERATORS:
-		...
-	    else:
-		raise PermissionError
-		
-	elif arg1 == "remove":
-	    if arg2 in OPERATORS:
-		...
-	    else:
-		raise PermissionError
-		
-	elif arg1 == "list":
-	    ...
-		
-	elif arg1 == None:
-	    ...
-		
-	else:
-	    ...
+
+        #-------------------------------------------------#
+        #                   Dc-Commands                   #
+        #-------------------------------------------------#
+
+
+        userid = ctx.author.id
+        OPERATORS = OPERATORS.append(userid)
+            
+        if arg1 == "set":
+            if arg2 in OPERATORS:
+                ...
+            else:
+                raise commands.PermissionError
+            
+        elif arg1 == "remove":
+            if arg2 in OPERATORS:
+                ...
+            else:
+                raise commands.PermissionError
+            
+        elif arg1 == "list":
+            ...
+            
+        elif arg1 == None:
+            ...
+            
+        else:
+            ...
 		
