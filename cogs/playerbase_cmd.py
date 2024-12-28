7dd856fe26933253d48bc133e3d17b3e676465a9
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-12-28 13:00:42 +0100

    Update playerbase_cmd.py

--- cogs/playerbase_cmd.py
+++ cogs/playerbase_cmd.py
@@ -171,7 +171,7 @@ class PlayerbaseCMD(commands.Cog):
                 try: # Soll einen Bug fixen, bei dem kein User gefundedn wird
                     self.user_cache[key] = discorduser.display_name
                 except:
-                    self.user_cache[key] = key
+                    self.user_cache[key] = str(key)
             discordname: str = self.user_cache[key]
             users.append([key, discordname.lower(), value])
         
