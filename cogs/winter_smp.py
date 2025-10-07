08adcfbdd386c126f33fd1fea2e848d296521020
Annhilati <78727320+annhilati@users.noreply.github.com>
2025-10-07 21:58:53 +0200

    Update winter_smp.py

--- cogs/winter_smp.py
+++ cogs/winter_smp.py
@@ -2,9 +2,8 @@ import discord
 from discord.ext import commands
 
 TARGET_CHANNEL: int = 1422662969233248327
-TARGET_MESSAGE: int = 1422959267102851212
 FAKE_MESSAGE: int = 1422959270512693392
-DEBUG_CHANNEL: int = 1424326556771418112
+DEBUG_CHANNEL: int = 955055841772470272
 
 class WinterSMP(commands.Cog):
     def __init__(self, bot):
@@ -12,10 +11,9 @@ class WinterSMP(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        print("Notice")
         member = payload.member
         message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
-
+        print(f"Winter SMP: {member.name} issued verification")
 
         if member.bot:
             return
@@ -23,19 +21,20 @@ class WinterSMP(commands.Cog):
         if payload.channel_id != TARGET_CHANNEL:
             return 
         
-        if message.id == TARGET_MESSAGE:
-            if str(payload.emoji) == "🔥":
-                await self.bot.get_channel(DEBUG_CHANNEL).send(f"{member.mention} hat mit 🔥 reagiert!")
-            await message.remove_reaction(payload.emoji, member)
-        elif message.id == FAKE_MESSAGE:   
+        if str(payload.emoji) == "🔥":
+            role = self.bot.get_guild(payload.guild_id).get_role(1329087875215527977)
+            await member.add_roles(role)
+            await self.bot.get_channel(DEBUG_CHANNEL).send(f"{member.mention} hat mit 🔥 reagiert!")
+        else:
             try:
                 await member.send(f"Wir vermuten, dass du das Briefing nicht gelesen hast. Bitte lies es nochmal. Falls du es tatsächlich schon gelesen hast, ignoriere diese Nachricht.")
             except discord.Forbidden:
                 await self.bot.get_channel(DEBUG_CHANNEL).send(f"{member.mention} konnte ich leider keine DM schicken.")
-            if payload.emoji.id != 1137799137497206854:
-                await message.remove_reaction(payload.emoji, member)
-        else:
+
+        if not (payload.emoji.id == 1137799137497206854 and message.id == FAKE_MESSAGE):
             await message.remove_reaction(payload.emoji, member)
+
+
 
 async def setup(bot):
     await bot.add_cog(WinterSMP(bot))
